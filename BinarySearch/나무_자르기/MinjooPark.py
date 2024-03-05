302dda4b18b95fb647b48eb21ef8175497e85524
Park-Minjoo <alswn9030@gmail.com>
2024-03-05 21:33:55 +0900

    [UPDATE] Cutting Trees - Minjoo Park

--- BinarySearch/나무_자르기/MinjooPark.py
+++ BinarySearch/나무_자르기/MinjooPark.py
@@ -1,19 +1,18 @@
-n, m = list(map(int, input().split(' ')))
-arr = list(map(int, input().split()))
+n, m = map(int, input().split())
+trees = list(map(int, input().split()))
 
-end = max(arr) # 19
-start = 0 # 10
-
+end = max(trees)
+start = 0
 
 result = 0
 while start <= end:
     count = 0
-    mid = (end + start) // 2
+    mid = (start + end) // 2
 
-    for i in arr:
+    for i in trees:
         if i > mid:
             count += i - mid
-    if count > m:
+    if count >= m:
         result = mid
         start = mid + 1
     else:
---
+++ dp/스티커/MinjooPark.py
@@ -0,0 +1,9 @@
+'''
+⭐️
+'''
+
+tc = int(input())
+for _ in range(tc):
+    n = int(input())
+    scores = [0] * n
+    scores = [list(map(int, input().split())) for _ in range(2)]
