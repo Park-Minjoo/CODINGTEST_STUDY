39f62576598d1e92c9b427274245441ffe19cd39
Park-Minjoo <alswn9030@gmail.com>
2024-03-15 15:45:14 +0900

    feat: Two pointers/블로그 문제풀이 - Minjoo Park (#10)

---
+++ Two Pointers/블로그/MinjooPark.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+n, x = map(int, input().split())
+arr = list(map(int, input().split()))
+
+if max(arr) == 0:
+    print("SAD")
+else:
+    arr_sum = sum(arr[0:x])
+    value = arr_sum
+    count = 1
+
+    for i in range(x, n):
+        # 슬라이딩 윈도우
+        value -= arr[i-x]
+        value += arr[i]
+        if value > arr_sum:
+            arr_sum = value
+            count = 1
+        elif value == arr_sum:
+            count += 1
+    print(arr_sum)
+    print(count)
+
+'''
+5 2
+1 4 2 5 1
+
+7 5
+1 1 1 1 1 5 1
+
+5 3
+0 0 0 0 0
+'''
