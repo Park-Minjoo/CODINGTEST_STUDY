df04beec445819ff511e00687a2b861e144816ae
Park-Minjoo <alswn9030@gmail.com>
2024-03-19 21:42:25 +0900

    feat: 크루스칼/전력난 문제풀이 - Minjoo Park (#10)

--- Kruskal/전력난/MinjooPark.py
+++ Kruskal/전력난/MinjooPark.py
@@ -1,3 +1,44 @@
+import sys
+input = sys.stdin.readline
+
+def find_parent(parent, x): # 특정 원소가 속한 집합
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b): # 두 원소가 속한 집합을 합치기
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+while True:
+    v, e = map(int, input().split())
+    if v == 0 and e == 0:
+        break
+
+    parent = [i for i in range(e)]
+    edges = []
+    result = 0
+
+    for _ in range(e): # 간선을 입력받음
+        a, b, cost = map(int, input().split())
+        edges.append((cost, a, b))
+        result += cost # 모든 cost를 더해줌
+
+    edges.sort() # cost를 중심으로 오름차순 정렬
+
+    for edge in edges:
+        cost, a, b = edge
+        if find_parent(parent, a) != find_parent(parent, b):
+            # 다르면 사이클 x
+            union_parent(parent, a, b)
+            result -= cost # 연결된 것을 빼줌
+
+    print(result)
+
+
 '''
 입력
 7 11
