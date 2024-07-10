59cead460e37c226b437fdbf4601a97d733ee748
Park-Minjoo <alswn9030@gmail.com>
2024-07-10 10:18:23 +0900

    feat: 오목 문제 풀이 (#55)

---
+++ Implementation/오목/MinjooPark.py
@@ -0,0 +1,66 @@
+import sys
+
+input = sys.stdin.readline
+
+board = []
+for i in range(19):
+    board.append(list(map(int, input().split())))
+
+dx = [0, 1, 1, -1]
+dy = [1, 0, 1, 1]
+
+for x in range(19):
+    for y in range(19):
+        if board[x][y] != 0: # 돌이 있는 칸
+            focus = board[x][y] # 돌의 색 저징
+
+            for i in range(4):
+                cnt = 1
+                nx = x + dx[i]
+                ny = y + dy[i]
+
+                while 0 <= nx < 19 and 0 <= ny < 19 and board[nx][ny] == focus: # 범위, 해당 방향에 돌이 있다면
+                    cnt += 1 # 하나 증가
+
+                    if cnt == 5: # 5가 되었음
+                        # 오목이 되었으나 육목 체크
+                        if 0 <= x - dx[i] < 19 and 0 <= y - dy[i] < 19 \
+                                and board[x - dx[i]][y - dy[i]] == focus: # 반대칸에 하나 더 있거나
+                            break
+                        if 0 <= x - dx[i] < 19 and 0 <= y - dy[i] < 19 \
+                                and board[nx + dx[i]][ny + dy[i]] == focus: # 앞에 한칸에 더 있으면
+                            break
+                        print(focus)
+                        print(x+1, y+1)
+                        sys.exit(0)
+
+                    nx += dx[i] # 해당 방향으로 전진
+                    ny += dy[i] # 해당 방향으로 전진
+print(0)
+
+'''
+Input
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 1 2 0 0 2 2 2 1 0 0 0 0 0 0 0 0 0 0
+0 0 1 2 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0
+0 0 0 1 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 1 2 2 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 1 1 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 2 1 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+
+Output
+1
+3 2
+'''
